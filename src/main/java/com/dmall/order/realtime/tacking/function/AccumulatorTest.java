c7e844509c45d47c1a2c53a5ffa08be08461d45f
jiang.huang <jiang.huang@dmall.com>
2016-12-07 14:12:40 +0800

    add files

---
+++ src/main/java/com/dmall/order/realtime/tacking/function/AccumulatorTest.java
@@ -0,0 +1,11 @@
+package com.dmall.order.realtime.tacking.function;
+
+/**
+ * @author: jiang.huang
+ * @create Date: 2016/11/29 Time: 14:08
+ */
+public class AccumulatorTest {
+    public static void main(String[] args) {
+    }
+}
+
--- src/main/scala/com/dmall/demo/Demo2.scala
+++ src/main/scala/com/dmall/demo/Demo2.scala
@@ -5,6 +5,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+import scala.collection.mutable
+
 /**
   * Created by kevin on 11/7/16.
   */
@@ -17,13 +19,38 @@ object Demo2 {
     }
 
     val sparkConf = new SparkConf().setAppName("demo").setMaster("local[2]")
-    val ssc = new StreamingContext(sparkConf, Seconds(30))
+    val ssc = new StreamingContext(sparkConf, Seconds(15))
 
     val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_ONLY_SER_2)
     val rows = lines.map(_.split(" "))
-    val notEmpty = rows.map(_.filter(x => !x.isEmpty()))
-    notEmpty.print()
+    val notEmpty = rows.map(_.filter(x => !x.isEmpty())).map((line:Array[String]) => (line.head,line.last))
+    val count = notEmpty.map((pair:(String,String)) => (pair._1,1)).reduceByKey(_ + _)
+    count.print()
     ssc.start()
     ssc.awaitTermination()
+  }
+
+  def statusGroup(x:Map[String,Int],y:Map[String,Int]):Map[String,Int] = {
+      var result:Map[String,Int] = Map()
+
+
+      val yhead: (String, Int) = y.head
+      var contains = false
+
+      x.foreach((pair:(String,Int)) => {
+        if(yhead._1.equals(pair._1)) {
+          result = result + (pair._1 -> (pair._2 + yhead._2))
+          contains = true
+        } else {
+          result = result + pair
+        }
+      })
+
+      if (!contains) {
+        result = result ++ y
+        result
+      } else {
+        result
+      }
   }
 }
